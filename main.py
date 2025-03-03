d4660fededcce24ba1183b3b7d374d3aab06c540
Akgnbas <irem.bas@std.bogazici.edu.tr>
2025-03-03 18:05:47 +0300

    assignment solution

--- main.py
+++ main.py
@@ -5,3 +5,41 @@ def int_to_roman(num):
     :param num: Integer value between 1 and 3999 inclusive.
     :return: A string representing the Roman numeral of the integer.
     """
+    roman=""
+    for i in range(num//1000):
+        roman+="M"
+
+    
+    if (num//100)%10==9:
+        roman+="CM"
+    elif (num//100)%10>=5:
+        roman+="D"+"C"*(((num//100)%10)-5)
+    elif (num//100)%10==4:
+        roman+="CD"
+    else:
+        roman+="C"*((num//100)%10)
+
+
+    if (num//10)%10==9:
+        roman+="XC"
+    elif (num//10)%10>=5:
+        roman+="L"+"X"*(((num//10)%10)-5)
+    elif (num//10)%10==4:
+        roman+="XL"
+    else:
+        roman+="X"*((num//10)%10)
+
+
+    if num%10==9:
+        roman+="IX"
+    elif num%10>=5:
+        roman+="V"+"I"*((num%10)-5)
+    elif num%10==4:
+        roman+="IV"
+    else:
+        roman+="I"*(num%10)
+
+
+    return roman
+
+
